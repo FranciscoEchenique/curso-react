0f18c026eb5b5bee01ffbb56d417cfc5fa54cd86
FranciscoEchenique <echeniquefrancisco127@gmail.com>
2022-10-17 15:26:13 -0300

    Ultimos cambios

--- 03-curso-react/src/App.jsx
+++ 03-curso-react/src/App.jsx
@@ -1,9 +1,10 @@
 import CounterApp from "./CounterApp";
+import './App.css';
 
 export default function App (){
     return(
         <div>
-            <CounterApp value={12}/>
+            <CounterApp value={0}/>
         </div>
     )
 }
