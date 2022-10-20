5ded876f1c34ab521d89b02fb58eee11c53572e1
FranciscoEchenique <echeniquefrancisco127@gmail.com>
2022-10-20 20:16:13 -0300

    Bug

--- 03-curso-react/tests/base-pruebas/11-async-await.test.js
+++
@@ -1,8 +0,0 @@
-import { getImagen } from "../../src/base-pruebas/11-async-await"
-
-describe('Pruebas en 11-async-await', () => {
-    test('getImagen debe retornar una imagen', async () => {
-        const url = await getImagen();
-        expect( typeof url ).toBe( 'string' )
-    })
-})
